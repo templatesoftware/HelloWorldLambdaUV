f7ff1c53b17a42565046632acd17d33557279f5f
template <56929958+cid219@users.noreply.github.com>
2024-11-24 14:49:45 -0500

    init

---
+++ src/hello_world_lambda_uv/handler.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+
+def handler(event, context):
+    # validate inputs - AWS_REGION must be set in the operating system environment
+    if 'AWS_REGION' not in os.environ:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "No configured region"}),
+        }
+    aws_region = os.environ['AWS_REGION']
+    # if name is present in the input event include it in our response message
+    if 'name' in event:
+        message = f"Hello {event['name']}! From {aws_region}."
+    else:
+        message = f"Hello from {aws_region}."
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": message
+        })
+    }
---
+++ tests/handler_test.py
@@ -0,0 +1,65 @@
+import os
+import unittest
+from hello_world_lambda_uv import handler
+from unittest.mock import patch
+
+
+class TestHandler(unittest.TestCase):
+
+    @patch.dict(os.environ, {'AWS_REGION': 'us-west-2'})
+    def test_region_parsing_from_env(self):
+        context = {
+            "function_version": "1.0",
+            "aws_request_id": "123456789"
+        }
+        event = {
+            "resource": "/example",
+            "httpMethod": "GET",
+            "queryStringParameters": {"key1": "value1"}
+        }
+        response = handler.handler(
+            event,
+            context
+        )
+        self.assertEqual(response["statusCode"], 200)
+        self.assertEqual(response["headers"], {'Content-Type': 'application/json'})
+        self.assertEqual(response["body"], "{\"message\": \"Hello from us-west-2.\"}")
+
+    @patch.dict(os.environ, {'AWS_REGION': 'us-east-1'})
+    def test_names_are_extracted_from_event(self):
+        context = {
+            "function_version": "1.0",
+            "aws_request_id": "123456789"
+        }
+        event = {
+            "resource": "/example",
+            "httpMethod": "GET",
+            "name": "John Smith",
+        }
+        response = handler.handler(
+            event,
+            context
+        )
+        self.assertEqual(response["statusCode"], 200)
+        self.assertEqual(response["headers"], {'Content-Type': 'application/json'})
+        self.assertEqual(response["body"], "{\"message\": \"Hello John Smith! From us-east-1.\"}")
+
+
+
+    def test_error_code_returned_when_no_region_set(self):
+        context = {
+            "function_version": "1.0",
+            "aws_request_id": "123456789"
+        }
+        event = {
+            "resource": "/example",
+            "httpMethod": "GET",
+            "queryStringParameters": {"key1": "value1"}
+        }
+        response = handler.handler(
+            event,
+            context
+        )
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], "{\"error\": \"No configured region\"}")
+
